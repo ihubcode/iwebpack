8e61a1087d7e1a1bb5947ac7e2e8e52560857a5e
robin.zhu <1987357952@qq.com>
2017-09-29 22:06:00 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const glob = require('glob');
+const webpack = require('webpack')
+
+const config = {
+  entry: {},
+  output: {},
+  module: {},
+  plugins: {}
+};
+module.exports = config;
